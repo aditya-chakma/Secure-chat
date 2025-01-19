7262d52f9181a395ad71c41a5477322ff5f996c9
Aditya Chakma <aditya.chakma@therapservices.net>
2025-01-19 14:56:53 +0600

    Added thymeleaf

--- Chat-server/src/main/java/com/iAxis/secure_chat/controller/UserController.java
+++ Chat-server/src/main/java/com/iAxis/secure_chat/controller/UserController.java
@@ -1,19 +1,16 @@
 package com.iAxis.secure_chat.controller;
 
-import com.iAxis.secure_chat.entity.User;
 import com.iAxis.secure_chat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author aditya.chakma
  * @since 18 Jan, 2025
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -24,9 +21,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/list")
-    public List<User> getUserList() {
-        return userService.findAll();
+    @GetMapping
+    public String create() {
+        return "user";
     }
 
 }
